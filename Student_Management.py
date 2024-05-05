46dc947d2366d26c0df12cc2055855bd451958ce
bunny <bunnyaluvala@gmail.com>
2024-05-04 23:03:23 -0400

    connected to SQL

--- Student_Management.py
+++ Student_Management.py
@@ -1,5 +1,5 @@
 from PyQt6.QtWidgets import QApplication, QVBoxLayout, QLabel, QWidget, QGridLayout, \
-    QLineEdit, QPushButton, QMainWindow, QGraphicsGridLayout, QTableWidget, QTableWidgetItem
+    QLineEdit, QPushButton, QMainWindow, QGraphicsGridLayout, QTableWidget, QTableWidgetItem, QDialog
 from PyQt6.QtGui import QAction
 import sys
 import sqlite3
@@ -14,6 +14,7 @@ class MainWindow(QMainWindow):
         help_menu_item = self.menuBar().addMenu("&Help")
 
         add_student_action = QAction("Add Student", self)
+        add_student_action.triggered.connect(self.insert)
         file_menu_item.addAction(add_student_action)
 
         about_action = QAction("About", self)
@@ -33,6 +34,26 @@ class MainWindow(QMainWindow):
             for column_number, data in enumerate(row_data):
                 self.table.setItem(row_number, column_number, QTableWidgetItem(str(data)))
         connection.close()
+
+    def insert(self):
+        dialog = InsertDialog()
+        dialog.exec()
+
+
+class InsertDialog(QDialog):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("Inset Student Data")
+        self.FixedWidth(300)
+        self.FixedHeight(300)
+
+        layout = QVBoxLayout()
+
+        student_name = QLineEdit()
+        student_name.setPlaceholderText("Name")
+        layout.addWidget(student_name)
+
+        self.setLayout(layout)
 
 
 app = QApplication(sys.argv)
