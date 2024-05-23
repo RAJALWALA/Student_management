eac60a20bb39c10426b5a44cdb7b50253bd29c2a
bunny <bunnyaluvala@gmail.com>
2024-05-23 01:12:26 -0400

    Finished

--- Student_Management.py
+++ Student_Management.py
@@ -28,6 +28,7 @@ class MainWindow(QMainWindow):
 
         about_action = QAction("About", self)
         help_menu_item.addAction(about_action)
+        about_action.triggered.connect(self.about)
 
         self.table = QTableWidget()
         self.table.setColumnCount(4)
@@ -89,6 +90,19 @@ class MainWindow(QMainWindow):
     def delete(self):
         dialog = DeleteDialog()
         dialog.exec()
+
+    def about(self):
+        dialog = AboutDialog()
+        dialog.exec()
+
+class AboutDialog(QMessageBox):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("About")
+        content = """
+        fell free to use this app 
+        """
+        self.setText(content)
 
 
 class EditDialog(QDialog):
